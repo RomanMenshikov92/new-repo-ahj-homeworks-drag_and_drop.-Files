790311433e9001f9cb61072a9f0b6c8836a730d0
Roman Menshikov <R-Tupac@yandex.ru>
2023-10-02 00:29:08 +0300

    hw-6

---
+++ webpack.prod.js
@@ -0,0 +1,18 @@
+const { merge } = require('webpack-merge');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const common = require('./webpack.common');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin({}),
+      new TerserPlugin({
+        extractComments: /^\**!|@preserve|@license|@cc_on/i,
+      }),
+      '...',
+    ],
+  },
+});
